6a82c8859ed5594f8d24f49da556864e9d790061
rachelyt98 <61983696+rachelyt98@users.noreply.github.com>
2020-03-10 00:38:17 +0800

    Grocery Project

---
+++ src/components/Counter.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <button v-on:click = "increment()">+</button>
+        <label>{{counter}}</label>
+        <button v-on:click = "decrement()">-</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            counter:0
+        }
+    },
+
+    methods: {
+        increment:function() {
+            this.counter++;
+        },
+        decrement:function() {
+            this.counter--;
+        }
+    }
+}
+</script>
